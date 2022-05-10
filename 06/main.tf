483de6de1df8df50cac4380976f0e1577f1b2dee
Junki Hori <junki326+github@gmail.com>
2022-05-10 19:08:35 +0900

    feat: 6.3 ログバケット AWSの各種サービスがログを保存するためのログバケットを作成

--- 06/main.tf
+++ 06/main.tf
@@ -40,3 +40,33 @@ resource "aws_s3_bucket" "public" {
     max_age_seconds = 3000
   }
 }
+
+resource "aws_s3_bucket" "alb_log" {
+  bucket = "alb-log-pragmatic-terraform-20220510"
+
+  lifecycle_rule {
+    enabled = true
+
+    expiration {
+      days = "180"
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "alb_log" {
+  bucket = aws_s3_bucket.alb_log.id
+  policy = data.aws_iam_policy_document.alb_log.json
+}
+
+data "aws_iam_policy_document" "alb_log" {
+  statement {
+    effect = "Allow"
+    actions = ["s3:PutObject"]
+    resources = ["arn:aws:s3:::${aws_s3_bucket.alb_log.id}/*"]
+
+    principals {
+      type = "AWS"
+      identifiers = ["582318560864"] # TOKYOリージョン https://docs.aws.amazon.com/ja_jp/elasticloadbalancing/latest/classic/enable-access-logs.html
+    }
+  }
+}
