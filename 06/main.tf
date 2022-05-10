3cd53946f0a5081778d7e85545305fdb946c9f92
Junki Hori <junki326+github@gmail.com>
2022-05-10 19:03:41 +0900

    feat: 6.2 パブリックバケット

--- 06/main.tf
+++ 06/main.tf
@@ -1,3 +1,4 @@
+# S3プライベートバケット
 resource "aws_s3_bucket" "private" {
   bucket = "private-pragmatic-terraform-20220510"
 
@@ -19,10 +20,23 @@ resource "aws_s3_bucket" "private" {
 # ブロックパブリックアクセス
 # 予期しないオブジェクトの公開を抑止できる。
 # 特別に理由がなければ全て有効にする。
-resource "aws_s3_bucket_puclic_access_block" "private" {
-  bucket = aws_s3_bucket.private.id
-  block_public_acls = true
-  block_public_policy = true
-  ignore_public_acls = true
+resource "aws_s3_bucket_public_access_block" "private" {
+  bucket                  = aws_s3_bucket.private.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
   restrict_public_buckets = true
+}
+
+# S3パブリックバケット
+resource "aws_s3_bucket" "public" {
+  bucket = "public-pragmatic-terraform-20220510"
+  acl    = "public-read"
+
+  cors_rule {
+    allowed_origins = ["https://example.com"]
+    allowed_methods = ["GET"]
+    allowed_headers = ["*"]
+    max_age_seconds = 3000
+  }
 }
