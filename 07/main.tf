38d81b377b1ce5d5c9650e8fcb03066d001c9d9b
Junki Hori <junki326+github@gmail.com>
2022-05-11 16:56:35 +0900

    feat: 7.3.1 パブリックネットワークのマルチAZ化

--- 07/main.tf
+++ 07/main.tf
@@ -29,8 +29,13 @@ resource "aws_route" "public" {
   destination_cidr_block = "0.0.0.0/0"
 }
 
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public.id
+resource "aws_route_table_association" "public_0" {
+  subnet_id      = aws_subnet.public_0.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_1.id
   route_table_id = aws_route_table.public.id
 }
 
@@ -65,4 +70,18 @@ resource "aws_route" "private" {
   route_table_id         = aws_route_table.private.id
   gateway_id             = aws_nat_gateway.example.id
   destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_subnet" "public_0" {
+  vpc_id                  = aws_vpc.example.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-northeast-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "public_1" {
+  vpc_id                  = aws_vpc.example.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "ap-northeast-1c"
+  map_public_ip_on_launch = true
 }
